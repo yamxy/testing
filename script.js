4bec5f06ce94956c39499fcec01c3a6a730017b8
yamxy <martinezyamile8@gmail.com>
2021-02-11 22:27:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,9 @@ function generate(){
 
 function myFunction() {
   var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } 
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
 }
